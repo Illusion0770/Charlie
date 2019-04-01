ca68109889de75462ae2f0e9feb2c420ef562295
Illusion0770 <luciferxarcangel@gmail.com>
2019-04-01 16:48:51 -0300

    fixes

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 <body>
 <div class="col-xs-12 principal">
     <div class="row">
-    
+
         <div class="col-md-6 col-sm-6 col-xs-12">
     <div class="x_panel">
         <div class="x_title">
