c236847be26dafcdfcbcc8307beff6e98f8bd7d7
Illusion0770 <luciferxarcangel@gmail.com>
2019-06-06 10:14:16 -0300

    difficult times

--- Charlie.php
+++ Charlie.php
@@ -7,7 +7,9 @@
  */
 $timestampAgora = time();
 try {
+
     /*CRIA ARRAY DE DIRETÓRIOS COM A ÚLTIMA DATA DE ALTERAÇÃO*/
+
     $aDirectories=glob("FTP/*",GLOB_ONLYDIR);
 
     $listaClientes=array();
@@ -39,6 +41,7 @@ try {
     }
 
     /*ORGANIZA O ARRAY DE DIRETÓRIOS POR DATA DE ALTERAÇÃO MAIS ANTIGO[0] >> MAIS NOVO[N]*/
+
     for($i = 0; $i < sizeof($listaClientes); $i++){
         for($j = 0; $j < (sizeof($listaClientes) - 1); $j++){
             if($listaClientes[$j]->Ordenador >= $listaClientes[$j+1]->Ordenador){
@@ -48,37 +51,59 @@ try {
             }
         }
     }
-    /*DA O NOME AOS PLACEHOLDERS DOS INPUTS DE NOME */
-    $json_dataCounter = 0;
-    $json_data = json_decode(file_get_contents('json/ArquivoSyncCharlie.json', true));
-//    print_r($json_data);
-    $json_data_object = $json_data[$json_dataCounter];
-    $test = get_object_vars($json_data_object);
-//    print_r($test['codigocliente'] .' - '. $test['nomecliente']);
 
+    /*DA O NOME AOS PLACEHOLDERS DOS INPUTS DE NOME */
+
+    $json_dataCounter   = 0;
+    $json_data          = json_decode(file_get_contents('json/ArquivoSyncCharlie.json', true));
+    $json_data_object   = $json_data[$json_dataCounter];
+    $test               = get_object_vars($json_data_object);
     /*VERIFICA SE FAZ MAIS DE 24/48 HORAS DESDE A ÚLTIMA INTEGRAÇÃO, MOSTRA AS INFORMAÇÕES NA TELA*/
+
     foreach($listaClientes as $cliente)
     {
-        $timestampCliente = strtotime($cliente->DataModificacao);
-        $diferenca = $timestampAgora - $timestampCliente;
-        $diferenca = $diferenca / 3600;
+        $timestampCliente   = strtotime($cliente->DataModificacao);
+        $diferenca          = $timestampAgora - $timestampCliente;
+        $diferenca          = $diferenca / 3600;
 
-        $json_data = json_decode(file_get_contents('json/ArquivoSyncCharlie.json', true));
-        $json_data_object = $json_data[$json_dataCounter];
-        $test = get_object_vars($json_data_object);
+        $json_data          = json_decode(file_get_contents('json/ArquivoSyncCharlie.json', true));
+        $json_data_object   = $json_data[$json_dataCounter];
+        $test               = get_object_vars($json_data_object);
+        $final_result       = new stdClass();
 
-//        var_dump($test);
         if($test['codigocliente'] === ''){
             $test['nomecliente']    = '';
             $test['codigocliente']  = '';
         }
+
+
         if($cliente->DataModificacao) {
+
+            $array_compared_object = array();
+
+            for($i = 0; $i < sizeof($json_data); $i++){
+                $json_data = json_decode(file_get_contents('json/ArquivoSyncCharlie.json', true));
+                $json_data_object = $json_data[$i];
+                $test = get_object_vars($json_data_object);
+                $compared_object = new stdClass();
+                $compared_object->Nome = $test['nomecliente'];
+                $compared_object->Codigo = $test['codigocliente'];
+                $compared_object->Pasta = $test['pasta'];
+                $array_compared_object[$i] = $compared_object;
+            }
+
             if ($diferenca > 48) {
                 $cliente->Cliente = utf8_encode($cliente->Cliente);
+                for($i = 0; $i < sizeof($json_data); $i++){
+                    if($array_compared_object[$i]->Pasta == basename($cliente->Cliente)){
+                        $final_result->Codigo   = $array_compared_object[$i]->Codigo;
+                        $final_result->Nome     = $array_compared_object[$i]->Nome;
+                    }
+                }
                 ?>
                 <tr>
                 <td>
-                    <input type="text" name="cliente" class="cliente" value="<?=$test['codigocliente'] . ' - ' . $test['nomecliente']?>">
+                    <input type="text" name="cliente" class="cliente" value="<?=$final_result->Codigo . ' - ' . $final_result->Nome?>">
                 </td>
                 <td class="pastas"><?= basename($cliente->Cliente) ?></td>
                 <td class="datasync">
@@ -88,11 +113,17 @@ try {
                 </tr><?php
                 $json_dataCounter++;
             } else if (24 < $diferenca && $diferenca <= 48) {
+                for($i = 0; $i < sizeof($json_data); $i++){
+                    if($array_compared_object[$i]->Pasta == basename($cliente->Cliente)){
+                        $final_result->Codigo   = $array_compared_object[$i]->Codigo;
+                        $final_result->Nome     = $array_compared_object[$i]->Nome;
+                    }
+                }
                 $cliente->Cliente = utf8_encode($cliente->Cliente);
                 ?>
                 <tr>
                 <td>
-                    <input type="text" name="cliente" class="cliente" placeholder="<?=$test['codigocliente'] . ' - ' . $test['nomecliente']?>">
+                    <input type="text" name="cliente" class="cliente" value="<?=$final_result->Codigo . ' - ' . $final_result->Nome?>">
                 </td>
                 <td class="pastas"><?= basename($cliente->Cliente) ?></td>
                 <td class="datasync">
@@ -102,11 +133,17 @@ try {
                 </tr><?php
                 $json_dataCounter++;
             } else if ($diferenca <= 24) {
+                for($i = 0; $i < sizeof($json_data); $i++){
+                    if($array_compared_object[$i]->Pasta == basename($cliente->Cliente)){
+                        $final_result->Codigo   = $array_compared_object[$i]->Codigo;
+                        $final_result->Nome     = $array_compared_object[$i]->Nome;
+                    }
+                }
                 $cliente->Cliente = utf8_encode($cliente->Cliente);
                 ?>
                 <tr>
                 <td>
-                    <input type="text" name="cliente" class="cliente" placeholder="<?=$test['codigocliente'] . ' - ' . $test['nomecliente']?>">                 </td>
+                    <input type="text" name="cliente" class="cliente" value="<?=$final_result->Codigo . ' - ' . $final_result->Nome?>">
                 <td class="pastas"><?= basename($cliente->Cliente) ?></td>
                 <td class="datasync">
                     <?=date_format(date_create_from_format('Y-m-d H:i:s', $cliente->DataModificacao),"d/m/Y H:i:s")?>
@@ -117,7 +154,6 @@ try {
             }
         }
     }
-
 
 } catch (UnexpectedValueException $e) {
     echo "Erro " . $e->getMessage();
--- sync/syncExterno.php
+++ sync/syncExterno.php
@@ -1,6 +1,24 @@
 <?php
 
-$json = json_decode($_POST['clientes']);
+$code = $_GET['codigo'];
 
+//$json_data          = json_decode($_POST['clientes']);
+$json_data          = json_decode(file_get_contents('../json/ArquivoSyncCharlie.json', true));
+$returned_Object    = new stdClass();
 
-print_r($json[0]);
+for($i = 0; $i < sizeof($json_data); $i++){
+
+    $test                   = get_object_vars($json_data[$i]);
+    $inside_object          = new stdClass();
+    $inside_object->Codigo  = $test['codigocliente'];
+    $inside_object->Data    = $test['data'];
+
+    if($code == $inside_object->Codigo){
+
+        $returned_Object->Codigo    = $inside_object->Codigo;
+        $returned_Object->Data      =  $inside_object->Data;
+
+    }
+}
+
+print_r($returned_Object->Data);
